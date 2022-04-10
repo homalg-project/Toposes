b0cf53781a24a1a0b2cd1776ac2a01ff9212f776
Mohamed Barakat <mohamed.barakat@rwth-aachen.de>
2022-04-10 14:21:02 +0200

    renamed files DistributiveCocartesian* -> CodistributiveCocartesian*

---
+++ gap/CodistributiveCocartesianCategoriesProperties.gd
@@ -0,0 +1,15 @@
+
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Toposes: Elementary toposes
+#
+# Declarations
+#
+
+AddCategoricalProperty( [ "IsCodistributiveCocartesianCategory", "IsDistributiveCartesianCategory" ] );
+
+CAP_INTERNAL_CONSTRUCTIVE_CATEGORIES_RECORD.IsCodistributiveCocartesianCategory  := Concatenation( [
+"LeftCocartesianCodistributivityFactoringWithGivenObjects",
+"RightCocartesianCodistributivityFactoringWithGivenObjects",
+],
+CAP_INTERNAL_CONSTRUCTIVE_CATEGORIES_RECORD.IsCocartesianCategory,
+CAP_INTERNAL_CONSTRUCTIVE_CATEGORIES_RECORD.IsCartesianCategory );
